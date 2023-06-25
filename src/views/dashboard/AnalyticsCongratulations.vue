d33fc31cfb9c148d93713070fccecd3bd432ef64
FaroukBel <fbelkhyate@gmail.com>
2023-06-25 12:35:42 +0100

    Init

---
+++ src/views/dashboard/AnalyticsCongratulations.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { useTheme } from 'vuetify'
+import illustrationJohnDark from '@images/cards/illustration-john-dark.png'
+import illustrationJohnLight from '@images/cards/illustration-john-light.png'
+import DemoSimpleTableFixedHeader from '@/views/pages/tables/DemoSimpleTableFixedHeader.vue'
+const { global } = useTheme()
+const illustrationJohn = computed(() => global.name.value === 'dark' ? illustrationJohnDark : illustrationJohnLight)
+</script>
+
+<template>
+  <VCard class="text-center text-sm-start">
+    <VRow no-gutters>
+      <VCol
+        cols="12"
+        sm="8"
+        order="2"
+        order-sm="1"
+      >
+   
+      <VCard title="Fixed Header">
+      
+        <DemoSimpleTableFixedHeader />
+      </VCard>
+
+        
+      </VCol>
+
+    </VRow>
+  </VCard>
+</template>
+
+<style lang="scss" scoped>
+.john-illustration {
+  inset-block-end: -0.0625rem;
+  inset-inline-end: 3rem;
+}
+</style>
+
+
+<script>
+  export default {
+    data() {
+      return {
+        items: [
+          { age: 40, first_name: 'Dickerson', last_name: 'Macdonald' },
+          { age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+          { age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+          { age: 38, first_name: 'Jami', last_name: 'Carney' }
+        ]
+      }
+    }
+  }
+</script>
